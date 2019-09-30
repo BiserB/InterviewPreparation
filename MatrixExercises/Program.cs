c6aea70f6d026bab9ed5ebda4722982b8f52f623
BiserBorisov <butoff@abv.bg>
2019-09-30 21:51:42 +0300

    Add real interview questions.

---
+++ MatrixExercises/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PrimeNumbersMatrix
+{
+    // Build matrix filled with prime numbers
+
+    class Program
+    {
+        private static int[,] matrix;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+
+            int num = int.Parse(Console.ReadLine());
+
+            List<int> primes = GetPrimes(num * num);
+
+            BuildMatrix(num, primes);
+
+            PrintMatrix();
+
+            Console.ReadKey();
+        }
+
+        private static void BuildMatrix(int num, List<int> primes)
+        {
+            matrix = new int[num, num];
+            int index = 0;
+
+            for (int r = 0; r < num; r++)
+            {
+                for (int c = 0; c < num; c++)
+                {
+                    matrix[r, c] = primes[index++];
+                }
+            }
+        }
+
+        private static List<int> GetPrimes(int count)
+        {
+            List<int> primes = new List<int>(count);
+
+            int candidate = 2;
+
+            while (primes.Count < count)
+            {
+                if (IsPrime(candidate))
+                {
+                    primes.Add(candidate);
+                }
+
+                candidate++;
+            }
+
+            return primes;
+        }
+
+        private static bool IsPrime(int num)
+        {
+            var maxDivider = Math.Sqrt(num);
+
+            for (int divider = 2; divider <= maxDivider; divider++)
+            {
+                if (num % divider == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static void PrintMatrix()
+        {
+            // align all numbers with adding indent
+
+            int maxLength = 0;
+
+            for (int r = 0; r < matrix.GetLength(0); r++)
+            {
+                for (int c = 0; c < matrix.GetLength(1); c++)
+                {
+                    int numberLength = matrix[r, c].ToString().Length;
+
+                    if (numberLength > maxLength)
+                    {
+                        maxLength = numberLength;
+                    }
+                }
+            }
+
+            maxLength++; // extra space
+
+            for (int r = 0; r < matrix.GetLength(0); r++)
+            {
+                for (int c = 0; c < matrix.GetLength(1); c++)
+                {
+                    Console.Write(matrix[r, c].ToString().PadLeft(maxLength));
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ SetsFromArray/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace SetsFromArray
+{
+    // Print all different multisets (set with duplicate elements) 
+    // containing K elements from a source integer set with N elements 
+    // where 1 <= K <= N; K <= 5; N <= 25.
+    // Example: Inputs: {1, 2, 3}, N = 3, K = 2
+    // Output: (1, 1), (1, 2), (1, 3), (2, 2), (2, 3), (3, 3)
+
+    class Program
+    {
+        private static string[] source;
+        private static int setSize;
+        private static string[] set;
+
+        static void Main()
+        {
+            Console.WriteLine("Enter set size:");
+            setSize = int.Parse(Console.ReadLine());
+
+            set = new string[setSize];
+
+            Console.WriteLine("Enter space separated elements:");
+            source = Console.ReadLine().Split();
+
+            Generate(0, 0);
+        }
+
+        static void Generate(int sourceIndex, int setIndex)
+        {
+            for (int i = sourceIndex; i < source.Length; i++)
+            {
+                set[setIndex] = source[i];
+
+                if (setIndex + 1 < setSize)
+                {
+                    Generate(i, setIndex + 1);
+                }
+                else
+                {
+                    Console.WriteLine(string.Join(",", set));
+                }
+            }
+        }
+    }
+}
